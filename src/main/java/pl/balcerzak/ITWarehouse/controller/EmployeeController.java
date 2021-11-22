6e73e2d56d38eb4c1c07cc9f63b9994010dc93b5
Rafał Balcerzak <rafalbalcerzak98@gmail.com>
2021-11-22 12:30:59 +0100

    - dodanie EmployeeController.java dla Employee - wystawienie api do pobierania oraz dodawania pracowników

---
+++ src/main/java/pl/balcerzak/ITWarehouse/controller/EmployeeController.java
@@ -0,0 +1,39 @@
+package pl.balcerzak.ITWarehouse.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pl.balcerzak.ITWarehouse.entity.Employee;
+import pl.balcerzak.ITWarehouse.service.EmployeeService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/employee")
+public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping
+    ResponseEntity<List<Employee>> getAllEmployees(){
+        return ResponseEntity.ok(employeeService.getAllEmployee());
+    }
+
+    @GetMapping("/{id}")
+    ResponseEntity<Employee> getEmployeeById(@PathVariable long id){
+        return ResponseEntity.ok(employeeService.getEmployeeById(id));
+    }
+
+    @PostMapping
+    ResponseEntity<Employee> addEmployee(@RequestBody Employee employee){
+        return ResponseEntity.ok(employeeService.addEmployee(employee));
+    }
+
+    @GetMapping("/company/{companyName}")
+    ResponseEntity<List<Employee>> getAllEmployeesByCompanyName(@PathVariable String companyName){
+        return ResponseEntity.ok(employeeService.getAllEmployeesByCompanyName(companyName));
+    }
+}
